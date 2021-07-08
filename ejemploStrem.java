8a704fd77fdeb3712add820aa1187b18bd8be663
alexsephiroth <alexsephiroth@gmail.com>
2021-07-07 21:30:03 -0500

    mi primer commit

---
+++ ejemploStrem.java
@@ -0,0 +1,12 @@
+public class ejemploStrem {
+    public static void main(String[] args) {
+        String cadena1= "esto es una cadena";
+        String cadena2= "hola";
+        System.out.println("longitud de la cadena: "+ cadena1.length());
+        System.out.println("funcion indexof :"+ cadena1.indexOf("e"));
+        System.out.println("funcion lastindexof :"+ cadena1.lastIndexOf("e"));
+        System.out.println("funcion charAT :"+ cadena2.charAt(1));
+        System.out.println("funcion equals :"+ cadena2.equals(cadena1));
+    }
+    
+}
