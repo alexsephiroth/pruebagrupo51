024b4cad9f88084cb6b0d896ef4ec13b9d4c5991
alexsephiroth <alexsephiroth@gmail.com>
2021-07-13 18:55:59 -0500

    sistema notas poo

--- clase04maven/src/main/java/co/edu/utp/misiontic2022/c2/App.java
+++ clase04maven/src/main/java/co/edu/utp/misiontic2022/c2/App.java
@@ -8,6 +8,40 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Nota notavacia = new Nota();
+        /*
+        System.out.println("notavacia");
+        System.out.println("valor escala100 es " + notavacia.getEscala100());
+        System.out.println("valor escala5 es " + notavacia.getEscala5());
+        System.out.println("valor cualitativa es " + notavacia.getCualitativa());
+        */
+        notavacia.mostrarNotasConsola();
+
+        Nota notaescala5= new Nota(4.0);
+        /*
+        System.out.println("nota escala5 ");
+        System.out.println("valor escala 5 " + notaescala5.getEscala5());
+        System.out.println("valor escala 100 " + notaescala5.getEscala100());
+        System.out.println("valor cualitativa " + notaescala5.getCualitativa());
+        */
+        notaescala5.mostrarNotasConsola();
+
+        Nota notaescala100= new Nota(60);
+        /*
+        System.out.println("nota escala100 ");
+        System.out.println("valor escala 5 " + notaescala100.getEscala5());
+        System.out.println("valor escala 100 " + notaescala100.getEscala100());
+        System.out.println("valor cualitativa " + notaescala100.getCualitativa());
+        */
+        notaescala100.mostrarNotasConsola();
+        
+        Nota notatodos= new Nota(100,5,"aprobado");
+        /*
+        System.out.println("nota todos ");
+        System.out.println("valor escala 5 " + notatodos.getEscala5());
+        System.out.println("valor escala 100 " + notatodos.getEscala100());
+        System.out.println("valor cualitativa " + notatodos.getCualitativa());
+        */
+        notatodos.mostrarNotasConsola();
     }
 }
--- clase04maven/src/main/java/co/edu/utp/misiontic2022/c2/Nota.java
+++ clase04maven/src/main/java/co/edu/utp/misiontic2022/c2/Nota.java
@@ -6,21 +6,74 @@ public class Nota{
     private double escala5;
     private String cualitativa;
     private String nombre;//evaluacion,quiz, reto   
+    //2  
     Nota (){
         this.escala100=0;
         this.escala5=0;
         this.cualitativa="";
     }
     
-    Nota(pEsacala100){
+    Nota(int pEsacala100){
         this.escala100=pEsacala100;
         this.escala5=pEsacala100/20;
 
-        if{pEscala100 >= 60}{
+        if(pEsacala100 >= 60){
             this.cualitativa="aprobado";
         }else{
             this.cualitativa="no aprobada";
         }
     }
-    
+    Nota(double pEsacala5){
+        this.escala5=pEsacala5;
+        this.escala5=(int)pEsacala5*20;
+
+        if(pEsacala5 >= 2.95){
+            this.cualitativa="aprobado";
+        }else{
+            this.cualitativa="no aprobada";
+        }
+
+
+    }
+    Nota(int pEsacala100,double pEsacala5,String pcualitativa){
+        this.escala100=pEsacala100;
+        this.escala5=pEsacala5;
+        this.cualitativa=pcualitativa;
+        
+    }
+    //3  metodos comportamiento
+    public void mostrarNotasConsola(){
+        System.out.println("------------------");
+        System.out.println("------info nota------");
+        System.out.println("nota escala100 ");
+        System.out.println("valor escala 5 " + this.escala5);
+        System.out.println("valor escala 100 " + this.escala100);
+        System.out.println("valor cualitativa " + this.cualitativa);
+    }
+
+    //4 get y set
+    public int getEscala100(){
+        return escala100;
+    }
+    public double getEscala5(){
+        return escala5;
+    }
+
+    public String getCualitativa() {
+        return cualitativa;
+    }
+
+    public void setEscala100(int nuevaEscala100){
+        this.escala100=nuevaEscala100;
+    }
+
+    public void setEscala5(double nuevaEscala5){
+        this.escala5=nuevaEscala5;
+    }
+
+    public void setCualitativa(String nuevaCualitativa){
+        this.cualitativa = nuevaCualitativa;
+    }
 }
+
+
